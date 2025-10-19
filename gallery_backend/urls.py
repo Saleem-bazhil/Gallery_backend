af698324418889b3ea27340d6e24e43557926508
saleem-bazhil <ab588538@gmail.com>
2025-10-19 16:37:43 +0530

    fix 404 page not found

--- gallery_backend/urls.py
+++ gallery_backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve 
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,3 +36,7 @@ if not settings.DEBUG:
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
+    
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='react_app'),
+]
